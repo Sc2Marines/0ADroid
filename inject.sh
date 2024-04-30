916a958cf6148e7309836ffb6d90178c6432f2d4
Sc2Marines <alexandre.foll35@gmail.com>
2024-04-30 21:14:19 +0200

    add remove

--- inject.sh
+++ inject.sh
@@ -5,4 +5,5 @@ cd ../..
 mkdir -p /home/alexa/.local/share/0ad/mods/0ADroid
 cp ./bot/0ADroid/mod.json /home/alexa/.local/share/0ad/mods/0ADroid/
 cp ./bot/0ADroid/0ADroid.zip /home/alexa/.local/share/0ad/mods/0ADroid/
+rm ./bot/0ADroid/0ADroid.zip
 echo "0ADroid has been injected into 0 A.D. mods directory"
